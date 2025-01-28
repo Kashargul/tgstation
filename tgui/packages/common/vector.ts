e7093ca09a85856c00641d6f484c04ab90876f6a
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-01-28 11:53:37 +0100

    functions

--- tgui/packages/common/vector.ts
+++ tgui/packages/common/vector.ts
@@ -8,7 +8,7 @@
  * @license MIT
  */
 
-function zip(arr: Vector[]):Vector[]{ return Array(Math.max(...arr.map(a => a.length))).fill().map((_,i) => arr.map(a => a[i]))};
+function zip(...arr: Vector[]):Vector[]{ return Array(Math.max(...arr.map(a => a.length))).fill().map((_,i) => arr.map(a => a[i]))};
 
 const ADD = (a: number, b: number): number => a + b;
 const SUB = (a: number, b: number): number => a - b;
@@ -17,35 +17,35 @@ const DIV = (a: number, b: number): number => a / b;
 
 export type Vector = number[];
 
-export const vecAdd = (...vecs: Vector[]): Vector => {
+export function vecAdd(...vecs: Vector[]): Vector {
   return zip(...vecs).map((x) => x.reduce(ADD));
 };
 
-export const vecSubtract = (...vecs: Vector[]): Vector => {
+export function vecSubtract(...vecs: Vector[]): Vector {
   return zip(...vecs).map((x) => x.reduce(SUB));
 };
 
-export const vecMultiply = (...vecs: Vector[]): Vector => {
+export function vecMultiply(...vecs: Vector[]): Vector {
   return zip(...vecs).map((x) => x.reduce(MUL));
 };
 
-export const vecDivide = (...vecs: Vector[]): Vector => {
+export function vecDivide(...vecs: Vector[]): Vector {
   return zip(...vecs).map((x) => x.reduce(DIV));
 };
 
-export const vecScale = (vec: Vector, n: number): Vector => {
+export function vecScale(vec: Vector, n: number): Vector {
   return vec.map((x) => x * n);
 };
 
-export const vecInverse = (vec: Vector): Vector => {
+export function vecInverse(vec: Vector): Vector {
   return vec.map((x) => -x);
 };
 
-export const vecLength = (vec: Vector): number => {
+export function vecLength(vec: Vector): number {
   return Math.sqrt(vecMultiply(vec, vec).reduce(ADD));
 };
 
-export const vecNormalize = (vec: Vector): Vector => {
+export function vecNormalize(vec: Vector): Vector {
   const length = vecLength(vec);
   return vec.map((c) => c / length);
 };
